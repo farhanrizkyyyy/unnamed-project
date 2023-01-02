94472a4de61be301271117a3ae3b5f2cdd841081
Farhan <farhanrizkyyyy@gmail.com>
2023-01-02 10:01:07 +0700

    add: create post, route, & showing route information

--- unnamed-client/main.js
+++ unnamed-client/main.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Feature, Map, View } from 'ol';
-import GeoJSON from 'ol/format/GeoJSON';
+import GeoJSON from 'ol/format/GeoJSON.js';
 import { Point } from 'ol/geom';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
@@ -18,6 +18,8 @@ var selectedCoordinates = {
 }
 
 var markersArray = new Array
+const baseUrl = 'http://127.0.0.1:3100/api'
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZhcmhhbnJpemt5eXl5IiwiaWF0IjoxNjcyNDg0MTUyLCJleHAiOjE2NzI0OTEzNTJ9.dNQZMYeRj-lYVVHrrHqw7BDjkK79VQBqs8wJcL_4wLo'
 
 const map = new Map({
   target: 'map',
@@ -25,11 +27,11 @@ const map = new Map({
     new TileLayer({
       source: new OSM()
     }),
-    // straitsLayer
   ],
   view: new View({
     // projection: 'EPSG:4326',
-    center: fromLonLat([107.679225, -6.746725]),
+    // center: [107.679225, -6.746725],
+    center: fromLonLat([107.679225, - 6.746725]),
     zoom: 9
   })
 });
@@ -37,6 +39,7 @@ const map = new Map({
 var markers = new VectorLayer({
   source: new VectorSource(),
   style: new Style({
+    // zIndex: 1000,
     image: new Icon({
       scale: .035,
       src: 'assets/marker.png'
@@ -44,25 +47,27 @@ var markers = new VectorLayer({
   })
 })
 
-axios.get('http://127.0.0.1:3100/api/posts').then((response) => {
+axios.get(`${baseUrl}/posts`).then((response) => {
   var array = response.data['data']
-  // console.log(array[0]['coordinate']['coordinates']);
-  array.forEach(element => {
-    markersArray.push(element['coordinate']['coordinates'])
+  array.forEach(e => {
+    markersArray.push(e['coordinate']['coordinates'])
   });
 
-  for (let i = 0; i < markersArray.length; i++) {
-    // console.log(markersArray[i]);
-    var marker = new Feature(new Point(fromLonLat(markersArray[i])))
+  markersArray.forEach(e => {
+    var marker = new Feature(new Point(fromLonLat(e)))
+    // console.log(marker);
     markers.getSource().addFeature(marker)
-  }
+  })
 
   map.addLayer(markers)
 })
 
 map.on('click', function (e) {
-  console.log(map.getCoordinateFromPixel(e.pixel));
-  var coordinate = map.getCoordinateFromPixel(e.pixel);
+  var coordinate = map.getCoordinateFromPixel(e.pixel)
+  console.log(coordinate);
+  document.getElementById('postLon').value = coordinate[0]
+  document.getElementById('postLat').value = coordinate[1]
+
   if (selectedCoordinates.start.length == 0) {
     selectedCoordinates.start = coordinate
     document.getElementById('fromLon').value = selectedCoordinates.start[0]
@@ -78,8 +83,7 @@ map.on('click', function (e) {
   const toLon = document.getElementById('toLon').value
   const toLat = document.getElementById('toLat').value
 
-  if (fromLon != null && fromLat != null && toLon != null && toLat != null) {
-    // console.log(selectedCoordinates);
+  if (fromLon != '' && fromLat != '' && toLon != '' && toLat != '') {
     createRoute(fromLon, fromLat, toLon, toLat)
   }
 })
@@ -87,33 +91,74 @@ map.on('click', function (e) {
 const line = new VectorLayer({
   source: new VectorSource(),
   style: new Style({
+    zIndex: 700,
     stroke: new Stroke({
-      width: 3,
-      color: 'black'
+      width: 7,
+      color: 'green',
+      lineDash: [10, 10]
     })
   })
 })
 
-function createRoute(fromLon, fromLat, toLon, toLat) {
+async function createRoute(fromLon, fromLat, toLon, toLat) {
   const apiKey = '5b3ce3597851110001cf62482e90325d33734b4193345a80836635aa'
-  return axios.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${fromLon},${fromLat}&end=${toLon},${toLat}`).then(
+  axios.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${fromLon},${fromLat}&end=${toLon},${toLat}`).then(
     (response) => {
-      console.log(response);
+      console.log(response.data);
 
       line.getSource().addFeatures(new GeoJSON().readFeatures(response.data))
       map.addLayer(line)
+      var features = response.data['features']
+      var segments = features[0]['properties']['segments']
+      var distance = segments[0]['distance']
+      var duration = segments[0]['duration']
+      document.getElementById('distance').innerHTML = `<p>Distance: ${distance}</p>`
+      document.getElementById('duration').innerHTML = `<p>Duration: ${duration}</p>`
     }
   )
+
+  axios.post(`${baseUrl}/route/create`, {
+    'lat_from': fromLat,
+    'long_from': fromLon,
+    'lat_to': toLat,
+    'long_to': toLon
+  }).then((response) => {
+    console.log(response.data);
+  })
 }
 
 document.getElementById('reset').onclick = function () {
   map.removeLayer(line)
-  document.getElementById('fromLon').value = null
-  document.getElementById('fromLat').value = null
-  document.getElementById('toLon').value = null
-  document.getElementById('toLat').value = null
+  document.getElementById('fromLon').value = ''
+  document.getElementById('fromLat').value = ''
+  document.getElementById('toLon').value = ''
+  document.getElementById('toLat').value = ''
   selectedCoordinates.start = []
   selectedCoordinates.end = []
+  document.getElementById('distance').innerHTML = `<p>Distance: </p>`
+  document.getElementById('duration').innerHTML = `<p>Duration: </p>`
 }
 
+document.getElementById('post-reset').onclick = function () {
+  document.getElementById('posts').value = ''
+}
 
+document.getElementById('post-btn').onclick = function () {
+  var description = document.getElementById('posts').value
+  var postLat = document.getElementById('postLat').value
+  var postLon = document.getElementById('postLon').value
+
+  if (description != '' && postLat != '' && postLon != '') {
+    axios.post(`${baseUrl}/post/create`, {
+      'description': description,
+      'lat': postLat,
+      'long': postLon
+    }, {
+      headers: {
+        'x-access-token': token
+      }
+    }).then((response) => {
+      console.log(response.data);
+    })
+  }
+}
